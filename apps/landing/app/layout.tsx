68736548a55431381c470d726ae054a21fa15251
Igor Miguel <igormiguelbs@gmail.com>
2025-10-29 22:36:52 -0300

    feat: google tag manager

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../src/styles/globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Allecto App",
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
+      <GoogleTagManager gtmId="GTM-PRXXLQHV" />
       <body>{children}</body>
     </html>
   );
